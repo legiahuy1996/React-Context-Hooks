2bc1d3091df0f2299d22282d7edcc28f78221ae4
Huy <legiahuy1996@outlook.com>
2020-02-16 08:51:25 +0700

    Creating and Consuming Mutiple Context

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,30 +1,35 @@
 import React, { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 
 class Navbar extends Component {
   static context = ThemeContext;
 
-  render() { 
+  render() {
     console.log(this.context);
-    return ( 
-      <ThemeContext.Consumer>{(context) => {
-          const { isLightTheme, light, dark } = context;
+    return (
+      <AuthContext.Consumer>{(authContext) => (
+        <ThemeContext.Consumer>{(themeContext) => {
+          const { isAuthenticated, toggleAuth } = authContext
+          const { isLightTheme, light, dark } = themeContext;
           const theme = isLightTheme ? light : dark;
           return (
-            <nav style={{background: theme.ui, color: theme.syntax} }>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
+            <nav style={{ background: theme.ui, color: theme.syntax }}>
+              <h1>Context App</h1>
+              <div onClick={toggleAuth}>{isAuthenticated ? 'log in' : 'log out'}</div>
+              <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+              </ul>
+            </nav>
           )
         }
-      }
-      </ThemeContext.Consumer>
+        }
+        </ThemeContext.Consumer>
+      )}</AuthContext.Consumer>
     );
   }
 }
- 
+
 export default Navbar;
