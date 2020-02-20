9b5c15bccfb9fa2082e9311d60292db0b6a3625e
Huy <legiahuy1996@outlook.com>
2020-02-20 21:17:47 +0700

    Multiple Contexts using Hooks

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,35 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-class Navbar extends Component {
-  static context = ThemeContext;
-
-  render() {
-    console.log(this.context);
-    return (
-      <AuthContext.Consumer>{(authContext) => (
-        <ThemeContext.Consumer>{(themeContext) => {
-          const { isAuthenticated, toggleAuth } = authContext
-          const { isLightTheme, light, dark } = themeContext;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.syntax }}>
-              <h1>Context App</h1>
-              <div onClick={toggleAuth}>{isAuthenticated ? 'log in' : 'log out'}</div>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          )
-        }
-        }
-        </ThemeContext.Consumer>
-      )}</AuthContext.Consumer>
-    );
-  }
+const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>{isAuthenticated ? 'log in' : 'log out'}</div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
 }
-
+ 
 export default Navbar;
